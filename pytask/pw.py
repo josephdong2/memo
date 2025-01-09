16ede7516a41455cb52d3f135268af0085157e50
jd <joseph.dong.2019@gmail.com>
2025-01-10 00:36:00 +0800

    withdraw pw utils avoiding danger

--- pytask/pw.py
+++
@@ -1,8 +0,0 @@
-# /bin/python3
-
-def add_to_keyring(a: list):
-    return f'''echo -n '{a[2]}' | secret-tool store --label="{a[0]}_{a[1]}" {a[0]} {a[1]}'''
-
-
-def change():
-    return f'''passwd'''
