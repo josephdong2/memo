89ae0faf0662c720711cf159b67992f00aca20d0
jd <joseph.dong.2019@gmail.com>
2025-01-12 00:31:54 +0800

    password encryption

--- pytask/file.py
+++ pytask/file.py
@@ -7,3 +7,12 @@ def find_between_D1_and_D2_in_DIR_matching_PATTERN(D1, D2, DIR, PATTERN):
 # todo
 def find_in_DIR_matching_PATTERN_then_sort_then_select_all_except_last_N(DIR, PATTERN, N):
     return f'''ls {DIR} | grep '{PATTERN}' | sort | head -n-{N}'''
+
+
+# todo
+def encrypt_at_FILEPATH_by_PWFILE_using_ALGO(FILEPATH, PWFILE, ALGO):
+    return f'''openssl {ALGO} -e -pbkdf2 -pass file:{PWFILE} < {FILEPATH}'''
+
+
+def decrypt_at_FILEPATH_by_PWFILE_using_ALGO(FILEPATH, PWFILE, ALGO):
+    return f'''openssl {ALGO} -d -pbkdf2 -pass file:{PWFILE} < {FILEPATH}'''
